15c65f77056c9ef231c6ba6a72ce40de6a4decbc
Jose Sánchez <raidrdev@gmail.com>
2021-10-11 19:00:49 +0200

    ADD: log in function and clear completed tasks

---
+++ firebase.js
@@ -0,0 +1,28 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAXVRA1SPH7hjDHqcr5sY7wONeJlc4rU6U",
+    authDomain: "todo-app-d5d21.firebaseapp.com",
+    projectId: "todo-app-d5d21",
+    storageBucket: "todo-app-d5d21.appspot.com",
+    messagingSenderId: "860763703255",
+    appId: "1:860763703255:web:e5b55ab992e2a6c9aaad92",
+    measurementId: "G-RYCFYDW9S0"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+var db = firebase.firestore();
+const auth = firebase.auth();
+
+function login(){
+
+    const userEmail = document.getElementById("email").value;
+    const userPassword = document.getElementById("password").value;
+
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).then((user) => {
+        console.log("Logeado");
+        location.replace("home.html");
+    }).catch((error) => {
+        window.alert(error);
+    })
+    
+}
